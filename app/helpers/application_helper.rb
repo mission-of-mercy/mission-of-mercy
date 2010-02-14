241819d9e24d311f5e9a1c28a3a04a7144f38e46
Jordan Byron <jordan@ducksoupsoftware.com>
2010-02-14 18:51:20 -0500

    Home and helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,18 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def production?
+    @is_production ||=(ENV['RAILS_ENV']=='production')
+  end
+
+  def version
+    '1.0'
+  end
+  
+  def real_currency(number)
+    number_to_currency(number,:delimiter => ",", :unit => "$ ",:separator => ".")
+  end
 end
