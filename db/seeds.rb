c0e6e6618de0f91fb514f654ac999ebb8286d1d2
Jordan Byron <jordan@ducksoupsoftware.com>
2010-02-28 19:44:41 -0500

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+## Users
+
+default_password = "temp123"
+total_xray_stations = 5
+
+users = [ {:login => "admin", :user_type => UserType::ADMIN},
+          {:login => "check_in", :user_type => UserType::CHECKIN},
+          {:login => "check_out", :user_type => UserType::CHECKOUT},
+          {:login => "assignment_desk", :user_type => UserType::ASSIGNMENT},
+          {:login => "pharmacy", :user_type => UserType::PHARMACY}]
+          
+(1..total_xray_stations).each do |id|
+  users << {  :login      => "xray_#{if}", 
+              :user_type  => UserType::XRAY,
+              :station_id => id}
+end
+
+users.each do |user|
+  User.create(  :login                  => user[:login], 
+                :user_type              => user[:user_type], 
+                :password               => default_password, 
+                :password_confirmation  => default_password,
+                :x_ray_station_id       => user[:station_id] )
+end
+
+## Treatment Area
+
+areas = [ {:name => "Radiology", :capacity => 50, :amalgam_composite_procedures => false},
+          {:name => "Hygiene", :capacity => 50, :amalgam_composite_procedures => false},
+          {:name => "Restoration", :capacity => 50, :amalgam_composite_procedures => false},
+          {:name => "Pediatrics", :capacity => 50, :amalgam_composite_procedures => false},
+          {:name => "Endodontics", :capacity => 50, :amalgam_composite_procedures => false},
+          {:name => "Surgery", :capacity => 50, :amalgam_composite_procedures => false}]
+
+areas.each do |area|
+  TreatementArea.create(area)
+end
