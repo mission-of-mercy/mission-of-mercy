b10a2ca79651321e9dacb8e6a256c4bbea53de67
Jordan Byron <jordan@ducksoupsoftware.com>
2010-04-10 17:40:32 -0400

    Clean up copyright notices and some references to CT

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,8 +23,12 @@ module ApplicationHelper
     @is_production ||=(ENV['RAILS_ENV']=='production')
   end
 
-  def version
-    '2.1'
+  def app_version
+    '2.5 Alpha'
+  end
+  
+  def clinic_state
+    "STATE"
   end
   
   def real_currency(number)
@@ -37,24 +41,6 @@ module ApplicationHelper
     elsif current_user.user_type == UserType::ADMIN
       "class='admin'"
     end
-  end
-  
-  def integrity_about
-    title = "Integrity Systems & Solutions"
-    
-    link_to_function title, 
-                     about(title, "integrity")
-  end
-  
-  def dss_about
-    title = "Duck Soup Software"
-    
-    link_to_function title, 
-                     about(title, "dss")
-  end
-  
-  def about(title, div_id)
-    "Modalbox.show($('#{div_id}'), {title: 'About #{title}', width: 650}); return false;"
   end
   
   def link_to_reset(form_id)
