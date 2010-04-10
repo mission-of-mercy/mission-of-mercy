e9e268774835614f37d1a3b50d5a12345667465a
Jordan Byron <jordan@ducksoupsoftware.com>
2010-04-10 17:29:57 -0400

    Fix the spelling of treatment area, because I suck at spelling

--- db/migrate/20100225205931_create_surveys.rb
+++ db/migrate/20100225205931_create_surveys.rb
@@ -1,26 +1,12 @@
 class CreateSurveys < ActiveRecord::Migration
   def self.up
     create_table :surveys do |t|
-      t.integer :time_spent_at_clinic_in_minutes
       t.string :city
       t.string :state
       t.string :zip
       t.integer :age
       t.string :sex
       t.string :race
-      t.boolean :pain
-      t.integer :pain_length_in_days
-      t.string :heard_about_clinic
-      t.boolean :told_needed_more_dental_treatment
-      t.boolean :has_place_to_be_seen_for_dental_care
-      t.boolean :no_insurance
-      t.boolean :insurance_from_job
-      t.boolean :medicaid_or_chp_plus
-      t.boolean :self_purchase_insurance
-      t.boolean :husky_insurance
-      t.boolean :saga_insurance
-      t.string :other_insurance
-      t.boolean :tobacco_use
       t.integer :rating_of_services
 
       t.timestamps
--- lib/user_type.rb
+++
@@ -1,6 +0,0 @@
-class UserType
-  Admin=1
-  CheckIn=2
-  CheckOut=3
-  XRay=4
-end
