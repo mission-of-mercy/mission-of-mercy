859790bebe60cfa4d12aec4d77b9f40a27635216
Jordan Byron <jordan@ducksoupsoftware.com>
2011-03-02 19:22:15 -0500

    Add haml to environment

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
 
   config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
+  config.gem 'haml', :version => '~> 3.0'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
