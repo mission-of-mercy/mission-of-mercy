20484debdc32acda60e85c3a1a81863b45b9fc41
Jordan Byron <jordan@ducksoupsoftware.com>
2011-03-27 13:45:01 -0400

    Add bundler and upgrade to rails 2.3.11

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source :gemcutter
+
+gem 'rails',         '~> 2.3.4'
+gem 'pg',            '~> 0.9.0'
+gem 'will_paginate', '~> 2.3.11'
+gem 'haml',          '~> 3.0'
+
+group :development do
+  
+end
+
+group :test do
+  
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,3 @@
-# Be sure to restart your server when you modify this file
-
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
-
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
@@ -14,14 +9,7 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
-  # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
-
-  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
-  config.gem 'haml', :version => '~> 3.0'
+  # Specify gems that this application depends on in Gemfile
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
