093ea2196b8d24f65e19abf5b7a9a3f9fd7c0a3c
Jordan Byron <jordan@ducksoupsoftware.com>
2011-04-04 21:25:54 -0400

    We don't need no stinkin SUDO

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :scm, :git
 set :user, "deploy"
 
 set :deploy_to, "/home/deploy/#{application}"
+set :use_sudo, false
 
 server "mom.integrityss.com", :app, :web, :db, :primary => true
 
@@ -12,7 +13,7 @@ namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
 
