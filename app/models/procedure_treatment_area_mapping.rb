e45239d32c9b40200d60d69600634209f8661403
Jordan Byron <jordan@ducksoupsoftware.com>
2011-04-09 12:42:20 -0400

    Set assigned when created mappings

--- app/models/procedure_treatment_area_mapping.rb
+++ app/models/procedure_treatment_area_mapping.rb
@@ -1,5 +1,6 @@
 class ProcedureTreatmentAreaMapping < ActiveRecord::Base
-  after_save :destroy_unless_assigned
+  before_create lambda {|m| m.assigned = true }
+  after_save    :destroy_unless_assigned
   
   belongs_to :procedure
   belongs_to :treatment_area
