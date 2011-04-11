aa04bad3b987fa03b149386e58f8da74a64fc8c6
Jordan Byron <jordan@ducksoupsoftware.com>
2011-04-11 18:16:05 -0400

    If patient in pain is true, time in pain field should be visible on admin page

--- public/javascripts/helpers.js
+++ public/javascripts/helpers.js
@@ -18,12 +18,14 @@ MoM.Helpers.showPatientSurvey = function (){
   return false;
 }
 
-MoM.Helpers.togglePatientPain = function(){
+MoM.Helpers.togglePatientPain = function(focus){
   var $ = jQuery;
+  
+  if(focus == undefined) focus = true;
   
   if($('#patient_pain_true').is(':checked') == true)
     $('#pain_length_div').slideDown(function(){
-      $('#patient_pain_length_in_days').focus();
+      if(focus) $('#patient_pain_length_in_days').focus();
     });
   else
     $('#pain_length_div').slideUp();
@@ -73,6 +75,9 @@ MoM.Helpers.checkIn = function(options){
   $('#date-input-toggle').click(function(){
     MoM.Helpers.toggleDateInput();
   })
+  
+  if($('#patient_pain_true').is(":checked"))
+    MoM.Helpers.togglePatientPain(false);
 }
 
 MoM.Helpers.toggleDateInput = function(){
