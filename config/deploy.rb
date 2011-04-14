47fe32f662ee349473d3bbfbd6656688e29220bf
Jordan Byron <jordan@ducksoupsoftware.com>
2011-04-14 16:51:35 -0400

    Previous Mom Clinics should be destroyed when a patient is removed

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,3 +30,28 @@ after 'deploy:update_code' do
      run "ln -nfs #{shared_path}/#{from} #{release_path}/#{to}"
    end
 end
+
+task :reset do
+  # Prompt to make really sure we want to reset
+  puts "\n\e[0;31m   ######################################################################" 
+  puts "   \n                Are you REALLY sure you want to reset ?"
+  puts "   \n                  Enter y/N + enter to continue\n   "
+  puts "   ######################################################################\e[0m\n" 
+  proceed = STDIN.gets[0..0] rescue nil 
+  exit unless proceed == 'y' || proceed == 'Y'
+  
+  reset_sql = %{
+    ALTER SEQUENCE patients_id_seq RESTART WITH 1;
+    ALTER SEQUENCE patient_procedures_id_seq RESTART WITH 1;
+    ALTER SEQUENCE patient_prescriptions_id_seq RESTART WITH 1;
+    ALTER SEQUENCE patient_pre_meds_id_seq RESTART WITH 1;
+    ALTER SEQUENCE surveys_id_seq RESTART WITH 1; }
+  
+  run "cd #{current_path} && ./script/console production" do |channel, stream, data|
+    channel.send_data("Patient.destroy_all\n")
+    channel.send_data("SupportRequest.destroy_all\n")
+    channel.send_data("Survey.destroy_all\n")
+    channel.send_data("Patient.connection.execute('#{reset_sql}')\n")
+    channel.send_data("exit\n")
+  end
+end
