fe51139d0ce9a1a3c51e7c524647a8e332ff8521
Jordan Byron <jordan@ducksoupsoftware.com>
2011-04-14 22:13:49 -0400

    Success! Now for some final tweaks

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,8 @@
 set :output, "#{Dir.pwd}/log/cron_log.log"
 
 every 15.minutes do
-  db_config  = YAML.load_file("#{Dir.pwd}/config/database.yml")
+  db_config  = YAML.load_file("#{Dir.pwd}/config/database.yml")["production"]
   mom_config = YAML.load_file("#{Dir.pwd}/config/mom.yml")
   
-  command %{pg_dump -i -h #{db_config["host"]} -U #{db_config["username"]} -p #{db_config["password"]} -F c -f "#{mom_config["dexis_path"]}/backup/Time.now.to_i.backup" #{db_config["database"]}}
+  command %{pg_dump -i -h #{db_config["host"]} -U #{db_config["username"]} -p #{db_config["password"]} -F c -f "#{mom_config["dexis_path"]}backup/Time.now.to_i.backup" #{db_config["database"]}}
 end
