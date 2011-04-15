06d3068b38927dbf0565bef5bf21a5930e3de9bf
Jordan Byron <jordan@ducksoupsoftware.com>
2011-04-14 22:19:02 -0400

    Now if only we can get this password working

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,5 +4,5 @@ every 15.minutes do
   db_config  = YAML.load_file("#{Dir.pwd}/config/database.yml")["production"]
   mom_config = YAML.load_file("#{Dir.pwd}/config/mom.yml")
   
-  command %{pg_dump -i -h #{db_config["host"]} -U #{db_config["username"]} -p #{db_config["password"]} -F c -f "#{mom_config["dexis_path"]}backup/Time.now.to_i.backup" #{db_config["database"]}}
+  command %{PGPASSWORD=#{db_config["password"]} pg_dump -i -h #{db_config["host"]} -U #{db_config["username"]} -F c -f "#{mom_config["dexis_path"]}backup/#{Time.now.to_i}.backup" #{db_config["database"]}}
 end
