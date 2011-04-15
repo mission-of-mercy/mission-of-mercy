bc2c86f6d5b3ba336430f1d00522308a759746fb
Jordan Byron <jordan@ducksoupsoftware.com>
2011-04-15 09:05:04 -0400

    Escape those special characters

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,5 +4,5 @@ every 15.minutes do
   db_config  = YAML.load_file("#{Dir.pwd}/config/database.yml")["production"]
   mom_config = YAML.load_file("#{Dir.pwd}/config/mom.yml")
   
-  command %{PGPASSWORD=#{db_config["password"]} pg_dump -i -h #{db_config["host"]} -U #{db_config["username"]} -F c -f "#{mom_config["dexis_path"]}backup/`date +%m_%d_%Y_%H_%M`.backup" #{db_config["database"]}}
+  command %{PGPASSWORD=#{db_config["password"]} pg_dump -i -h #{db_config["host"]} -U #{db_config["username"]} -F c -f "#{mom_config["dexis_path"]}backup/`date \+\%m_\%d_\%Y_\%H_\%M`.backup" #{db_config["database"]}}
 end
