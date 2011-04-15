6ffad8ee8b9011c10fa47d673b76971dfc38fdf7
Jordan Byron <jordan@ducksoupsoftware.com>
2011-04-14 22:10:31 -0400

    Getting fancy again

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,11 @@
-set :output, ":path/log/cron_log.log"
+require "futils"
+require "yaml"
+
+set :output, "#{Dir.pwd}/log/cron_log.log"
 
 every 15.minutes do
-  db_config  = YAML.load_file(":path/config/database.yml")
-  mom_config = YAML.load_file(":path/config/mom.yml")
+  db_config  = YAML.load_file("#{Dir.pwd}/config/database.yml")
+  mom_config = YAML.load_file("#{Dir.pwd}/config/mom.yml")
   
   command %{pg_dump -i -h #{db_config["host"]} -U #{db_config["username"]} -p #{db_config["password"]} -F c -f "#{mom_config["dexis_path"]}/backup/Time.now.to_i.backup" #{db_config["database"]}}
 end
