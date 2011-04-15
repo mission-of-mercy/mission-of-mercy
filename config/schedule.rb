9aafac2ed150c25e9f115cb6a8fbcf51f6c93791
Jordan Byron <jordan@ducksoupsoftware.com>
2011-04-14 22:05:13 -0400

    Don't try to be fancy

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :output, "#{Rails.root}/log/cron_log.log"
 
 every 15.minutes do
-  db_config  = Rails::Configuration.new.database_configuration["production"]
+  db_config  = YAML.load_file("#{Rails.root}/config/database.yml")
   mom_config = YAML.load_file("#{Rails.root}/config/mom.yml")
   
   command %{pg_dump -i -h #{db_config["host"]} -U #{db_config["username"]} -p #{db_config["password"]} -F c -f "#{mom_config["dexis_path"]}/backup/Time.now.to_i.backup" #{db_config["database"]}}
