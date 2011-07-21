212f22b95c4863a808c5aac81316a7e07ec1a7a1
Jordan Byron <jordan@ducksoupsoftware.com>
2011-07-21 10:35:09 -0400

    Allow exporting all patient records to csv

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,11 +1,11 @@
 class Admin::ReportsController < ApplicationController
   before_filter :admin_required
   before_filter :set_current_tab
-  
+
   def index
 
   end
-  
+
   def clinic_summary
     if params[:day] && params[:span]
       @report = Reports::ClinicSummary.new(params[:day], params[:span])
@@ -13,41 +13,45 @@ class Admin::ReportsController < ApplicationController
       @report = Reports::ClinicSummary.new
     end
   end
-  
+
   def treatment_area_distribution
     @areas            = TreatmentArea.all(:order => "name")
-    
+
     @current_capacity = @areas.map do |a|
       count = a.patients.count || 0
       count = Patient.count(:conditions => {:radiology => true}) if a.radiology?
       [a.name, count]
     end
   end
-  
+
   def post_clinic
     @report = Reports::PostClinic.new
   end
-  
+
   def export_patients
-    patients = Patient.find(params[:patients])
-    
+    if params[:patients]
+      patients = Patient.find(params[:patients])
+    else
+      patients = Patient.all
+    end
+
     csv_string = FasterCSV.generate do |csv|
       csv << ["Chart #", "First Name", "Last Name", "Date of Birth", "Phone",
               "Street Address", "City", "State", "Zip", "Sex"]
       patients.sort_by(&:id).each do |patient|
-        csv << [ patient.id, patient.first_name, patient.last_name, 
+        csv << [ patient.id, patient.first_name, patient.last_name,
                  patient.date_of_birth, patient.phone, patient.street,
                  patient.city, patient.state, patient.zip, patient.sex ]
       end
     end
-    
+
     send_data csv_string,
               :type => 'text/csv; charset=iso-8859-1; header=present',
               :disposition => "attachment; filename=#{params[:filename] || 'patients'}.csv"
   end
-  
+
   private
-  
+
   def set_current_tab
     @current_tab = "reports"
   end
