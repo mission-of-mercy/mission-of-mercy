89435477dcf0938a03a40ab1f1584ff943ac398b
Jordan Byron <jordan@ducksoupsoftware.com>
2011-08-09 20:40:04 +0800

    Also check in the pharmacy controller

--- app/controllers/pharmacy_controller.rb
+++ app/controllers/pharmacy_controller.rb
@@ -1,37 +1,37 @@
 class PharmacyController < ApplicationController
   before_filter :login_required
-    
+
   def check_out
     @patient = Patient.find(params[:patient_id])
-    
-    @patient.patient_prescriptions.each do |p| 
+
+    @patient.patient_prescriptions.each do |p|
       p.prescribed = true
     end
-    
+
     Prescription.all.each do |pres|
       unless @patient.prescriptions.exists? pres
         @patient.patient_prescriptions.build(:prescription_id => pres.id)
       end
     end
-    
-    @prescriptions = @patient.patient_prescriptions.sort_by {|p| p.prescription.position }
+
+    @prescriptions = @patient.patient_prescriptions.sort_by {|p| p.prescription.position || -1 }
   end
 
   def check_out_complete
     @patient = Patient.find(params[:patient_id])
-    
+
     @patient.attributes = params[:patient]
-    
+
     @patient.patient_prescriptions.each do |p|
       p.destroy if !p.new_record? && p.prescribed == "0"
     end
-    
+
     @patient.save
-    
+
     if @patient.patient_prescriptions.count > 0
-      @patient.flows.create(:area_id => ClinicArea::PHARMACY) 
+      @patient.flows.create(:area_id => ClinicArea::PHARMACY)
     end
-    
+
     flash[:notice] = "Prescriptions sucessfully added to patient's record"
     redirect_to patients_path
   end
