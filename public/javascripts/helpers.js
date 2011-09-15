42c6dd092d24ad00008d64b41f88a97bbc654603
Chris Kottom <chris@chriskottom.com>
2011-09-15 19:50:32 +0200

    Fix issue #32 - Checkin warnings should only be displayed on the first panel
    Modified existing JS helper to hide the #errorExplanation div when needed

--- public/javascripts/helpers.js
+++ public/javascripts/helpers.js
@@ -3,15 +3,23 @@ MoM.setupNamespace('Helpers');
 MoM.Helpers.showPatientDemographics = function(){
   $('demographics').show();
   $('bottom_demographics').show();
+  if($('errorExplanation')) {
+    $('errorExplanation').show();
+  }
+
   $('survey').hide();
   $('bottom_survey').hide();
-  
+
   return false;
 }
 
 MoM.Helpers.showPatientSurvey = function (){
   $('demographics').hide();
   $('bottom_demographics').hide();
+  if($('errorExplanation')) {
+    $('errorExplanation').hide();
+  }
+
   $('survey').show();
   $('bottom_survey').show();
   
