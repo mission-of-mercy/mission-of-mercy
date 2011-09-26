7776d6e22dd0cf9e61a5803f47be6aa08663a426
Jordan Byron <jordan@ducksoupsoftware.com>
2011-09-26 11:22:36 -0400

    Inline if statements in js

--- public/javascripts/helpers.js
+++ public/javascripts/helpers.js
@@ -3,9 +3,7 @@ MoM.setupNamespace('Helpers');
 MoM.Helpers.showPatientDemographics = function(){
   $('demographics').show();
   $('bottom_demographics').show();
-  if($('errorExplanation')) {
-    $('errorExplanation').show();
-  }
+  if($('errorExplanation')) $('errorExplanation').show();
 
   $('survey').hide();
   $('bottom_survey').hide();
@@ -16,21 +14,19 @@ MoM.Helpers.showPatientDemographics = function(){
 MoM.Helpers.showPatientSurvey = function (){
   $('demographics').hide();
   $('bottom_demographics').hide();
-  if($('errorExplanation')) {
-    $('errorExplanation').hide();
-  }
+  if($('errorExplanation')) $('errorExplanation').hide();
 
   $('survey').show();
   $('bottom_survey').show();
-  
+
   return false;
 }
 
 MoM.Helpers.togglePatientPain = function(focus){
   var $ = jQuery;
-  
+
   if(focus == undefined) focus = true;
-  
+
   if($('#patient_pain_true').is(':checked') == true)
     $('#pain_length_div').slideDown(function(){
       if(focus) $('#patient_pain_length_in_days').focus();
@@ -43,7 +39,7 @@ MoM.Helpers.togglePatientPain = function(focus){
 
 MoM.Helpers.toggleOtherRace = function(){
   var $ = jQuery;
-  
+
   if($('#patient_race').val() == 'Other')
     $('#race_other_div').slideDown(function(){
       $('#patient_race_other').focus();
@@ -54,7 +50,7 @@ MoM.Helpers.toggleOtherRace = function(){
 
 MoM.Helpers.togglePreviousMoM = function(){
   var $ = jQuery;
-  
+
   if($('#patient_attended_previous_mom_event_true').is(':checked') == true)
     $('#previous_mom_location_div').slideDown();
   else
@@ -63,36 +59,36 @@ MoM.Helpers.togglePreviousMoM = function(){
 
 MoM.Helpers.checkIn = function(options){
   var $ = jQuery;
-  
+
   $('#patient_attended_previous_mom_event_true').change(function(e){
     MoM.Helpers.togglePreviousMoM();
   });
-  
+
   $('#patient_attended_previous_mom_event_false').change(function(e){
     MoM.Helpers.togglePreviousMoM();
   });
-  
+
   $('#patient_zip').keyup(function(){
     if($('#patient_zip').val().length >= 5)
       MoM.Helpers.lookupZip();
   });
-  
+
   if(options.dateInput == "text")
     MoM.Helpers.useTextDate();
   else
     MoM.Helpers.useSelectDate();
-    
+
   $('#date-input-toggle').click(function(){
     MoM.Helpers.toggleDateInput();
   })
-  
+
   if($('#patient_pain_true').is(":checked"))
     MoM.Helpers.togglePatientPain(false);
 }
 
 MoM.Helpers.toggleDateInput = function(){
   var $ = jQuery;
-  
+
   if($('#date-select').is(":visible"))
     MoM.Helpers.useTextDate();
   else
@@ -101,42 +97,42 @@ MoM.Helpers.toggleDateInput = function(){
 
 MoM.Helpers.useTextDate = function(){
   var $ = jQuery;
-  
+
   $('#date-select').hide();
-  
+
   $('body').append($('#date-select'));
   $('#date-input-container').append($('#date-text'));
   $('#date-text').show();
-  
+
   $('#date_input').val('text');
-  
+
   $('#date-format').slideDown();
 }
 
 MoM.Helpers.useSelectDate = function(){
   var $ = jQuery;
-  
+
   $('#date-text').hide();
-  
+
   $('body').append($('#date-text'));
   $('#date-input-container').append($('#date-select'));
   $('#date-select').show();
-  
+
   $('#date_input').val('select');
-  
+
   $('#date-format').slideUp();
 }
 
 MoM.Helpers.lookupZip = function(){
   var $ = jQuery;
-  
+
   $('#zip-spinner').show();
   $.getJSON("/patients/lookup_zip.json", {
     zip: $('#patient_zip').val()
-  }, 
+  },
   function(data){
     $('#zip-spinner').hide();
-    
+
     if(data.found == true){
       $('#patient_city').val(data.city);
       $('#patient_state').val(data.state);
