350674207f838086336743fb46ef62e6b9e630d6
Jordan Byron <jordan@ducksoupsoftware.com>
2011-10-21 19:06:31 -0400

    Remove restful_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,41 +1,38 @@
 class ApplicationController < ActionController::Base
-  include AuthenticatedSystem
-  
-  helper :all
   helper_method :app_config, :stats
-  
+
   protect_from_forgery
-  
+
   before_filter :set_area_id
   around_filter :setup_stats
 
   attr_accessor :current_area_id, :current_treatment_area_id
 
   private
-  
-  def set_area_id    
+
+  def set_area_id
     self.current_area_id = current_user.user_type if current_user
-    
+
     if treatment_id = params[:treatment_area_id]
       self.current_treatment_area_id = treatment_id
     else
       self.current_treatment_area_id = nil
     end
   end
-    
+
   def app_config
     @app_config ||= YAML.load_file("#{Rails.root}/config/mom.yml")
   end
-  
+
   def stats
     @stats
   end
-  
+
   def setup_stats
     @stats ||= Stats.new(session[:stats])
-    
+
     yield
-    
+
     session[:stats] = @stats.data
   end
 end
