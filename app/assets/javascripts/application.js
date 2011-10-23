796560eed47ac87eee99c83afcaf36b4472221ec
Jordan Byron <jordan@ducksoupsoftware.com>
2011-10-23 10:20:25 -0400

    Remove jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,20 +1,6 @@
-// This is a manifest file that'll be compiled into including all the files listed below.
-// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
-// be included in the compiled file accessible from http://example.com/assets/application.js
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
-// Setup the MoM Namespace
-var MoM = MoM ? MoM : new Object();
-
-MoM.setupNamespace = function(namespace){
-	if(MoM[namespace] == undefined)
-		MoM[namespace] = {}
-}
 
 MoM.init = function(auth_token){
   MoM.AuthToken = auth_token;
