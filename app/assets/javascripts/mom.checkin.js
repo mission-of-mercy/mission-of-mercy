0c8b5b84cdaaab6fe6b99b4c0de09fa2010767df
Jordan Byron <jordan@ducksoupsoftware.com>
2011-10-23 17:32:27 -0400

    Move print patient chart into MoM.Checkin

--- app/assets/javascripts/mom.checkin.js
+++ app/assets/javascripts/mom.checkin.js
@@ -1,6 +1,6 @@
 MoM.setupNamespace("Checkin");
 
-MoM.Checkin.init = function(){
+MoM.Checkin.init = function(lastPatient){
   $('#patient_first_name').focus();
 
   MoM.disableEnterKey();
@@ -8,4 +8,15 @@ MoM.Checkin.init = function(){
   $('#patient_survey_attributes_heard_about_clinic').change(function(e){
     showOtherHeardAbout();
   });
+
+  if(lastPatient){
+    MoM.Checkin.printChart(lastPatient)
+
+    // TODO Restore ModalBox
+    //Modalbox.show($('last_patient'), {title: "Patient\'s Chart Number", width: 300});}
+  }
+}
+
+MoM.Checkin.printChart = function(patientId){
+  MoM.openInBackground('/patients/' + patientId + '/print');
 }
