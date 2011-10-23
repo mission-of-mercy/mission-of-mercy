014a5558a25d10622b5686f14cb833871de5a4cb
Jordan Byron <jordan@ducksoupsoftware.com>
2011-10-23 16:42:52 -0400

    Restore status updates

--- app/models/support_request.rb
+++ app/models/support_request.rb
@@ -3,13 +3,11 @@ class SupportRequest < ActiveRecord::Base
   belongs_to :treatment_area
 
   def station_description
-    des = "<b>"
+    des = []
+    des << treatment_area.name if treatment_area
+    des << user.name if user
 
-    des += treatment_area.name if treatment_area
-    des += " #{user.name}" if user
-    #des += " (#{ip_address})"
-
-    des + "</b>"
+    des.compact.join(" ")
   end
 
   def to_hash
