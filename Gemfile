e015609f376117efa64459be976745552d3c512a
Jordan Byron <jordan@ducksoupsoftware.com>
2011-12-18 11:10:24 -0500

    Minor consistency / code format improvements

--- Gemfile
+++ Gemfile
@@ -7,20 +7,20 @@ gem 'haml', '~> 3.0'
 
 # TODO Re-enable asset pipeline
 # group :assets do
-#   gem 'sass-rails', "  ~> 3.1.0"
-#   gem 'coffee-rails', "~> 3.1.0"
+#   gem 'sass-rails', '  ~> 3.1.0'
+#   gem 'coffee-rails', '~> 3.1.0'
 #   gem 'uglifier'
 # end
 
 gem 'will_paginate', '~> 3.0.2'
-gem "devise"
+gem 'devise'
 gem 'dynamic_form'
 
 gem 'jquery-rails'
 
 group :test do
-  gem "minitest",  "~> 2.3.1"
-  gem "faker", '~> 0.9.5'
-  gem "factory_girl_rails"
+  gem 'minitest',  '~> 2.3.1'
+  gem 'faker', '~> 0.9.5'
+  gem 'factory_girl_rails'
   gem 'colorific', '~> 1.0.0'
 end
--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module MissionOfMercy
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Eastern Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -44,8 +44,6 @@ module MissionOfMercy
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    config.time_zone = 'Eastern Time (US & Canada)'
 
     Date::DATE_FORMATS.merge!(:default => '%m/%d/%Y')
 
