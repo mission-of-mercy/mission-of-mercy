a2cedba90a9a36e43d80a7d4a67286f5982fdde8
Jordan Byron <jordan@ducksoupsoftware.com>
2011-12-18 21:16:30 -0500

    Restore redirect after login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   before_filter :authenticate_user!
+
   def index
-    ## needs to implement this feature later
-    #redirect_to current_user.start_path if current_user && current_user.start_path != root_path
+    redirect_to current_user.start_path if current_user && current_user.start_path != root_path
   end
 
 end
