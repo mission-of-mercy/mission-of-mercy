fa6be67fcbbab612214ba7a9bec15b25c9b71a72
kbl <m.pietraszek@gmail.com>
2012-01-16 15:50:35 +0100

    not checked out scope for assignments

--- test/factories/patient_assignment_factory.rb
+++ test/factories/patient_assignment_factory.rb
@@ -2,6 +2,5 @@ FactoryGirl.define do
   factory :patient_assignment do
     patient
     treatment_area
-    checked_out_at { Date.today }
   end
 end
--- test/unit/patient_assignment_test.rb
+++ test/unit/patient_assignment_test.rb
@@ -2,9 +2,11 @@ require 'test_helper'
 
 class PatientAssignmentTest < ActiveSupport::TestCase
 
-  def test_factory_should_build_valid_assignment
-    a = Factory.build(:patient_assignment)
-    assert a.valid?
+  def test_should_return_not_checked_out_entries
+    a1 = Factory(:patient_assignment)
+    a2 = Factory(:patient_assignment, checked_out_at: Time.now)
+
+    assert_equal [a1], PatientAssignment.not_checked_out
   end
 
 end
