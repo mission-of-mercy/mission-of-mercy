1efc6bd0556e684d89a0e092159506711732c0b0
Jordan Byron <jordan.byron@gmail.com>
2012-03-22 19:52:49 -0400

    Sort users with numbers in logical order

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -101,5 +101,11 @@ module UsersHelper
       :method => :post,
       :id     => "help_link"
   end
+  
+  def user_list
+    User.order(:login).map {|u| [u.name, u.login] }.
+    sort_by { |x| x[0].split(%r{(?<=\D)(?=\d)|(?<=\d)(?=\D)}).
+    map{|x| (x =~ /\A\d+\z/) ? x.to_i : x } }
+  end
 
 end
