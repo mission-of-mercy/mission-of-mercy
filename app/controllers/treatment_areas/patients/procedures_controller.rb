3aa36d0be7b915b21b97f31e692ac09cfb1241e9
Chris Mohr <chris.mohr@gmail.com>
2012-10-10 21:47:00 -0700

    Adding the ability to add mulitple procedures with a comma separate tooth list
     - Added two integrational tests to cover the happy and negative paths
     - Prevent any procedures from being added if any errors occur

--- app/controllers/treatment_areas/patients/procedures_controller.rb
+++ app/controllers/treatment_areas/patients/procedures_controller.rb
@@ -20,23 +20,45 @@ class TreatmentAreas::Patients::ProceduresController < ApplicationController
   end
 
   def create
-    @patient_procedure = PatientProcedure.new(params[:patient_procedure])
+    tooth_numbers = params[:patient_procedure].delete(:tooth_number)
+    teeth = list_to_array(tooth_numbers)
 
-    if @patient_procedure.save
+    procedures  = add_multiple_procedures(params[:patient_procedure], teeth)
+    mark_procedures_as_added(procedures)
 
-      stats.procedure_added
-
-      @patient_procedure = @patient.patient_procedures.build
-
-      redirect_to treatment_area_patient_procedures_path(:procedure_added => true)
-    else
+    if procedures.empty?
+      @patient_procedure = PatientProcedure.new(params[:patient_procedure].
+                                            merge(:tooth_number => tooth_numbers))
+      @patient_procedure.valid?
       @selected_procedure = params[:patient_procedure][:procedure_id]
-
+      params[:patient_procedure][:tooth_number] = tooth_numbers
       render :action => :index
+    else
+      redirect_to treatment_area_patient_procedures_path(:procedure_added => true)
     end
   end
 
   private
+
+  def list_to_array(comma_list)
+    comma_list.split(/\s*,\s*/)
+  end
+
+  def add_multiple_procedures(procedure_params, teeth)
+    teeth = [nil] if teeth.empty? # ensure we create at least one
+    PatientProcedure.transaction do
+      teeth.map do |tooth_number|
+        procedure_params[:tooth_number] = tooth_number
+        PatientProcedure.create!(procedure_params)
+      end
+    end
+  rescue ActiveRecord::RecordInvalid => e
+    []
+  end
+
+  def mark_procedures_as_added(procedures)
+    procedures.length.times  { stats.procedure_added }
+  end
 
   def find_treatment_area
     @treatment_area = TreatmentArea.find(params[:treatment_area_id])
