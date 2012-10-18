7c7a90c6f733f1224d5b98a6e6b8f1f329d0932d
Ruy Diaz <diazruy@gmail.com>
2012-10-17 21:18:15 -0700

    Fill in data points with zero values

--- app/helpers/clinic_summary_helper.rb
+++ app/helpers/clinic_summary_helper.rb
@@ -1,7 +1,21 @@
 module ClinicSummaryHelper
   def procedures_per_hour(procedures)
+    start_time = procedures.first.hour
+    end_time = procedures.last.hour
+
+    total_hours = ((end_time - start_time)/3600 + 1).to_i
+
+    data = {}
+    total_hours.times do |index|
+      data[start_time + index.hours] = 0
+    end
+
     procedures.collect do |p|
-      [p.hour, p.total]
+      data[p.hour] = p.total
+    end
+
+    data.collect do |k, v|
+      [k.strftime('%Y/%m/%d %H:%M'), v]
     end
   end
 end
