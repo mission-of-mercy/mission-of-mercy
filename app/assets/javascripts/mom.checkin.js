a00accbf5cab723e80eaf511ff832f6e84fe0da9
Brent Vatne <brentvatne@gmail.com>
2012-10-17 21:57:51 -0700

    fix bug: could not click tabbar link out of patients edit

--- app/assets/javascripts/mom.checkin.js
+++ app/assets/javascripts/mom.checkin.js
@@ -4,20 +4,22 @@ MoM.Checkin.init = function(options){
 
   MoM.disableEnterKey($('form.new_patient'));
 
-  MoM.Checkin.hacks();
+  MoM.Checkin.hacks(options.editMode);
 
-  if (options.requireWaiverConfirmation)
+  if (options.requireWaiverConfirmation) {
     MoM.Checkin.disableAllFields();
-  else
-    MoM.Checkin.waiverConfirmed();
 
-  if(options.lastPatient.contactInformation == null)
+    $("#waiver_agree_button").click(function(e) {
+      MoM.Checkin.waiverConfirmed();
+      e.preventDefault();
+    });
+  } else {
+    MoM.Checkin.waiverConfirmed();
+  }
+
+  if(options.lastPatient.contactInformation == null) {
     MoM.Checkin.hidePreviousContactInformationButton();
-
-  $("#waiver_agree_button").click(function(e) {
-    MoM.Checkin.waiverConfirmed();
-    e.preventDefault();
-  });
+  }
 
   $('#patient_survey_attributes_heard_about_clinic').change(function(e){
     MoM.Checkin.toggleOtherHeardAbout();
@@ -105,34 +107,36 @@ MoM.Checkin.init = function(options){
     $('#waiver_agree_button').focus();
 }
 
-MoM.Checkin.hacks = function(){
-  $('#tabnav a').on('click', function(e){
-    var input = $(this)
-    var tab   = input.attr('data-tab');
+MoM.Checkin.hacks = function(editMode){
+  if (editMode == false) {
+    $('#tabnav a').on('click', function(e){
+      var input = $(this)
+      var tab   = input.attr('data-tab');
 
-    $('#tabnav li').removeClass('current');
+      $('#tabnav li').removeClass('current');
 
-    if(tab == "new"){
-      $('form.new_patient').show();
-      $('#reprint').hide();
-      $('#previous-patient').hide();
-      $('#waiver_agree_button').focus();
-    } else if(tab == "previous"){
-      $('form.new_patient').hide();
-      $('#previous-patient').show();
-      $('#reprint').hide();
-      $('#previous-patient input:first').focus();
-    } else if(tab == "print"){
-      $('form.new_patient').hide();
-      $('#previous-patient').hide();
-      $('#reprint').show();
-      $('#reprint input:first').focus();
-    }
+      if(tab == "new"){
+        $('form.new_patient').show();
+        $('#reprint').hide();
+        $('#previous-patient').hide();
+        $('#waiver_agree_button').focus();
+      } else if(tab == "previous"){
+        $('form.new_patient').hide();
+        $('#previous-patient').show();
+        $('#reprint').hide();
+        $('#previous-patient input:first').focus();
+      } else if(tab == "print"){
+        $('form.new_patient').hide();
+        $('#previous-patient').hide();
+        $('#reprint').show();
+        $('#reprint input:first').focus();
+      }
 
-    input.parent().addClass("current");
+      input.parent().addClass("current");
 
-    e.preventDefault();
-  });
+      e.preventDefault();
+    });
+  }
 
   $('#reprint').on('submit', function(e){
     var chartNumber = $('#reprint #chart_number').val();
