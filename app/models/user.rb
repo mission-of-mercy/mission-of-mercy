37a527e69d682370e89d72374800abc6d0e81fb3
Jordan Byron <jordan.byron@gmail.com>
2013-01-19 17:47:46 -0500

    More view cleanups

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,8 +24,7 @@ class User < ActiveRecord::Base
     elsif user_type == UserType::XRAY
       Rails.application.routes.url_helpers.treatment_area_patients_path(TreatmentArea.radiology)
     elsif user_type == UserType::CHECKOUT
-      # FIXME route to treatment_areas#index
-      Rails.application.routes.url_helpers.root_path
+      Rails.application.routes.url_helpers.treatment_areas_path
     elsif user_type == UserType::PHARMACY
       # FIXME route to pharmacy#index
       Rails.application.routes.url_helpers.patients_path
