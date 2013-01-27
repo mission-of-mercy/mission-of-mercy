e304cb62cb6165dc90791fc521e2bf4abbca86ed
Brent Vatne <brentvatne@gmail.com>
2013-01-27 13:59:04 -0800

    Refactor PatientSearch using Virtus, add treatment_area_id parameter

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'devise'
 gem 'dynamic_form'
 gem 'draper'
 gem 'rack-pjax'
+gem 'virtus'
 
 gem 'jquery-rails'
 gem 'will_paginate-bootstrap'
--- app/models/patient_search.rb
+++ app/models/patient_search.rb
@@ -1,39 +1,42 @@
 class PatientSearch
+  include Virtus
 
-  def initialize(params={})
-    @params = params
-  end
+  attribute :chart_number,      Integer
+  attribute :treatment_area_id, Integer
+  attribute :name,              String
+  attribute :commit,            String
 
   def execute
-    if params[:commit] == "Clear"
-      @params = {}
+    if blank_search? || commit == 'Clear'
+      return reset_attributes && Patient.none
+    elsif chart_number.present?
+      return Patient.where(id: chart_number)
     end
 
-    chart_number = params[:chart_number]
-    name         = params[:name]
+    patients = Patient.scoped
 
-    patients = if chart_number.present?
-      Patient.where(id: chart_number)
-    elsif name.present?
-      Patient.where('first_name ILIKE :name or last_name ILIKE :name',
-        name: "%#{name}%")
-    else
-      @params = {}
-      Patient.none
+    if name.present?
+      patients = patients.where(
+        'first_name ILIKE :name or last_name ILIKE :name', name: "%#{name}%"
+      )
+    end
+
+    if treatment_area_id.present?
+      patients = patients.joins(:treatment_areas).where(
+        'treatment_areas.id = ?', treatment_area_id
+      )
     end
 
     patients.order('id')
   end
 
-  def []=(key, value)
-    @params[key] = value
+  def reset_attributes
+    attributes.each do |key, value|
+      self[key] = nil
+    end
   end
 
-  def [](key)
-    params[key]
+  def blank_search?
+    attributes.values.all?(&:blank?)
   end
-
-  private
-  attr_reader :params
-
 end
