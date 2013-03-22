5664245a4c4abcb371c30daa7074cfc36b8a88c4
Jordan Byron <jordan.byron@gmail.com>
2013-03-22 18:13:14 -0400

    rand for a unique field probably isn't a good idea

--- test/factories.rb
+++ test/factories.rb
@@ -55,7 +55,7 @@ FactoryGirl.define do
 
   factory :procedure do
     description           { Faker::Lorem.words(1 + rand(4)).join(" ").titleize }
-    code                  { rand(1000) }
+    sequence(:code)       { |n| n }
     requires_tooth_number false
     requires_surface_code false
     cost                  { rand(500) }
