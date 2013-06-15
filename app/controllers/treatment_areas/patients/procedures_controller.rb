6116c6978a0e5fbfe99891bd7a3a59b920894319
Jordan Byron <jordan.byron@gmail.com>
2013-06-15 11:05:11 -0400

    Fast tooth number entry

--- app/controllers/treatment_areas/patients/procedures_controller.rb
+++ app/controllers/treatment_areas/patients/procedures_controller.rb
@@ -20,19 +20,32 @@ class TreatmentAreas::Patients::ProceduresController < ApplicationController
   end
 
   def create
-    @patient_procedure = PatientProcedure.new(params[:patient_procedure])
+    if tooth_numbers = params[:patient_procedure].delete(:tooth_numbers)
+      tooth_numbers.each do |tooth|
+        PatientProcedure.create(params[:patient_procedure].
+          merge(tooth_number: tooth))
 
-    if @patient_procedure.save
-
-      stats.procedure_added
+        stats.procedure_added
+      end
 
       @patient_procedure = @patient.patient_procedures.build
 
       redirect_to treatment_area_patient_procedures_path(:procedure_added => true)
     else
-      @selected_procedure = params[:patient_procedure][:procedure_id]
+      @patient_procedure = PatientProcedure.new(params[:patient_procedure])
 
-      render :action => :index
+      if @patient_procedure.save
+
+        stats.procedure_added
+
+        @patient_procedure = @patient.patient_procedures.build
+
+        redirect_to treatment_area_patient_procedures_path(:procedure_added => true)
+      else
+        @selected_procedure = params[:patient_procedure][:procedure_id]
+
+        render :action => :index
+      end
     end
   end
 
