bdd19f8cdff3a9789da000ddccb41b0690e066f3
Jordan Byron <jordan.byron@gmail.com>
2014-04-25 07:53:47 -0400

    And write a test to prove that the treatment area is set
    Yay test :sparkles:

--- test/integration/support_request_test.rb
+++ test/integration/support_request_test.rb
@@ -18,6 +18,22 @@ feature "Need Help" do
       [request.description, request.created_at]
   end
 
+  it "tracks the current treatment area if present" do
+    treatment_area = FactoryGirl.create(:treatment_area)
+
+    sign_in_as "Check out"
+
+    click_link treatment_area.name
+
+    click_link "help_link"
+
+    page.must_have_content "Help is on the way"
+
+    request = SupportRequest.first
+
+    request.treatment_area.must_equal treatment_area
+  end
+
   it "can be canceled" do
     sign_in_as "Check in"
 
