6b77130f1b904c3e094c3db20fa620ed51db5af4
Jordan Byron <jordan.byron@gmail.com>
2014-04-25 07:46:54 -0400

    Please set the current treatment area

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # after_filter :set_current_treatment_area
+  after_filter :set_current_treatment_area
 
   private
 
