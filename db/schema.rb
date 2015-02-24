e1f3e5a39e912cc34c8779b997d0de46264b25d1
Jordan Byron <jordan.byron@gmail.com>
2015-02-24 11:16:45 -0500

    Sometimes you work on a project for the first time in a long time ...
    And there are random uncommitted changes that just gotta be saved

--- db/schema.rb
+++ db/schema.rb
@@ -18,31 +18,31 @@ ActiveRecord::Schema.define(version: 20140429144117) do
 
   create_table "heard_about_clinics", force: true do |t|
     t.string   "reason"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "patient_assignments", force: true do |t|
     t.integer  "patient_id"
     t.integer  "treatment_area_id"
     t.datetime "checked_out_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "patient_flows", force: true do |t|
     t.integer  "treatment_area_id"
     t.integer  "patient_id"
     t.integer  "area_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "patient_pre_meds", force: true do |t|
     t.integer  "patient_id"
     t.integer  "pre_med_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean  "prescribed"
   end
 
@@ -50,8 +50,8 @@ ActiveRecord::Schema.define(version: 20140429144117) do
     t.integer  "patient_id"
     t.integer  "prescription_id"
     t.boolean  "prescribed"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_index "patient_prescriptions", ["patient_id"], name: "index_patient_prescriptions_on_patient_id", using: :btree
@@ -61,8 +61,8 @@ ActiveRecord::Schema.define(version: 20140429144117) do
     t.integer  "patient_id"
     t.string   "location"
     t.integer  "clinic_year"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.boolean  "attended"
   end
 
@@ -72,8 +72,8 @@ ActiveRecord::Schema.define(version: 20140429144117) do
     t.string   "tooth_number"
     t.string   "surface_code"
     t.integer  "provider_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   add_index "patient_procedures", ["patient_id"], name: "index_patient_procedures_on_patient_id", using: :btree
@@ -85,8 +85,8 @@ ActiveRecord::Schema.define(version: 20140429144117) do
     t.string   "state"
     t.string   "latitude"
     t.string   "longitude"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "county"
   end
 
@@ -109,8 +109,8 @@ ActiveRecord::Schema.define(version: 20140429144117) do
     t.string   "last_dental_visit"
     t.boolean  "pain"
     t.integer  "pain_length_in_days"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                             null: false
+    t.datetime "updated_at",                                             null: false
     t.integer  "survey_id"
     t.string   "phone"
     t.integer  "previous_chart_number"
@@ -122,8 +122,8 @@ ActiveRecord::Schema.define(version: 20140429144117) do
   create_table "pre_meds", force: true do |t|
     t.string   "description"
     t.float    "cost"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "prescriptions", force: true do |t|
@@ -132,8 +132,8 @@ ActiveRecord::Schema.define(version: 20140429144117) do
     t.integer  "quantity"
     t.string   "dosage"
     t.float    "cost"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "position"
   end
 
@@ -147,8 +147,8 @@ ActiveRecord::Schema.define(version: 20140429144117) do
   create_table "procedure_treatment_area_mappings", force: true do |t|
     t.integer  "procedure_id"
     t.integer  "treatment_area_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.boolean  "assigned"
   end
 
@@ -161,14 +161,14 @@ ActiveRecord::Schema.define(version: 20140429144117) do
     t.boolean  "auto_add"
     t.float    "cost"
     t.integer  "number_of_surfaces"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
   create_table "races", force: true do |t|
     t.string   "category"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "support_requests", force: true do |t|
@@ -178,8 +178,8 @@ ActiveRecord::Schema.define(version: 20140429144117) do
     t.string   "ip_address"
     t.boolean  "resolved"
     t.datetime "resolved_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "surveys", force: true do |t|
@@ -190,8 +190,8 @@ ActiveRecord::Schema.define(version: 20140429144117) do
     t.string   "sex"
     t.string   "race"
     t.integer  "rating_of_services"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.boolean  "pain"
     t.integer  "pain_length_in_days"
     t.string   "heard_about_clinic"
@@ -213,15 +213,15 @@ ActiveRecord::Schema.define(version: 20140429144117) do
   create_table "treatment_areas", force: true do |t|
     t.string   "name"
     t.integer  "capacity"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.boolean  "amalgam_composite_procedures"
   end
 
   create_table "treatments", force: true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
     t.boolean  "provided",   default: true
   end
 
@@ -235,8 +235,8 @@ ActiveRecord::Schema.define(version: 20140429144117) do
     t.datetime "remember_token_expires_at"
     t.integer  "x_ray_station_id"
     t.integer  "user_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
     t.string   "encrypted_password",        limit: 128,              null: false
   end
 
