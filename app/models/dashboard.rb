8b3268542844a69dbc0b566e05560ad52a7c31cf
Jordan Byron <jordan.byron@gmail.com>
2015-03-20 13:26:54 -0400

    Make sure check out numbers are unique

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -13,8 +13,6 @@ class Dashboard
     expected_close = in_clinic.order("created_at DESC").first.
                        try(:expected_check_out_time)
     expected_close = expected_close ? expected_close.strftime("%I:%M %p") : nil
-    # These aren't taking into account repeat patients
-    # Which makes me :sad:
     check_outs = PatientFlow.where(area_id: ClinicArea::CHECKOUT).
       where("patients.previous_chart_number is null").
       joins("LEFT JOIN patients ON patients.id = patient_flows.patient_id").
